3f48b767d0fbf03fdc93c823502c6c9cbe3e9e5f
Avinash Kumar Singh <avkumars@adobe.com>
2019-12-17 02:31:21 +0530

    Adding latest changes

--- src/main/java/com/avinash/configuration/WebConfig.java
+++ src/main/java/com/avinash/configuration/WebConfig.java
@@ -3,10 +3,11 @@ package com.avinash.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-
+@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Override
@@ -14,7 +15,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	    configurer.favorPathExtension(false).
 	            favorParameter(true).
 	            parameterName("mediaType").
-	            ignoreAcceptHeader(true).
+	            ignoreAcceptHeader(false).
 	            useJaf(false).
 	            defaultContentType(MediaType.APPLICATION_XML).
 	            mediaType("xml", MediaType.APPLICATION_XML).
